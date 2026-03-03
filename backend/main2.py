e11cd1964a2d4d6470eff87c494920b041aa0f2e
Raz <razbinkarkidoli@gmail.com>
2026-03-03 15:23:30 -0800

    add vercel url to CORS

--- backend/main2.py
+++ backend/main2.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://egghead-ai-tau.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
