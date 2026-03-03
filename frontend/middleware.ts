c64aaf00afe1c931904d637cf6518f9441fa3034
Raz <razbinkarkidoli@gmail.com>
2026-03-03 14:28:03 -0800

    remove duplicate middleware

--- frontend/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0";
-
-export default withMiddlewareAuthRequired();
-
-export const config = {
-  matcher: ["/dashboard/:path*", "/api/:path*"],
-};
